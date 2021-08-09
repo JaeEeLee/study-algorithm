d4d02cb483466ececf169240f396ec15bd797624
wodl0504 <wodl0504@naver.com>
2021-08-09 23:59:34 +0900

    2021-08-09 알고리즘

---
+++ src/algorithm/stairCase.js
@@ -0,0 +1,43 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'staircase' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
+function staircase(n) {
+    // Write your code here
+    var res = ''
+    for(var i = 1; i <= n; i++ ){
+        res += ' '.repeat(n - i) + '#'.repeat(i) + "\n"
+    }
+    
+    console.log(res)
+}
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+
+    staircase(n);
+}
