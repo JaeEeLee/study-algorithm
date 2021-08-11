60d10e6ccb35b2d190f020d8153f149025823ba7
wodl0504 <wodl0504@naver.com>
2021-08-11 23:28:29 +0900

    2021-08-11 style customizing

--- src/Main.tsx
+++ src/Main.tsx
@@ -7,6 +7,9 @@ const Main: React.FC = () => (
 		<Switch>
 			<Route path="/home" component={Home} />
 			<Route path="/test" render={() => <h3>teststesters</h3>} />
+			<Route path="/baeck" render={() => <h3>백준 알고리즘</h3>} />
+			<Route path="//hacker-rank" render={() => <h3>해커랭크</h3>} />
+			<Route path="/memo" render={() => <h3>memo</h3>} />
 			<Redirect path="*" to="/home" />
 		</Switch>
 	</BrowserRouter>
