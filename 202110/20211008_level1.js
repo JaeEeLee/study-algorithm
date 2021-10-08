3b2600a727d0beb73bd9f422dd0f82296fba4d07
LeeJaeEe <wodl0504@naver.com>
2021-10-08 23:34:56 +0900

    시저 암호 구하기

--- 202110/20211008_level1.js
+++ 202110/20211008_level1.js
@@ -1,31 +1,43 @@
 // #1
 // 시저 암호 구하기
 // 20211008 진행중
-function secretCode() {
-	solution();
-	function solution(s, n) {
-		const a = alphabet();
-		let obj = [];
-		s.split('').map((v) => {
-			let num = a.indexOf(v);
-			let str = a.substr(num + n, 1);
-			if (isUpperCase(v)) str = str.toUpperCase();
+const res = solution('a B z', 4);
+// const res = solution('AB', 1)
+console.log(res);
 
-			obj.push(str);
-		});
+function solution(s, n) {
+	const abc = alphabet();
+	let obj = [];
 
-		return obj.join('');
+	s.split('').map((v) => {
+		if (v === ' ') return obj.push(' ');
+
+		let chk = isUpperCase(v);
+		if (chk) v = v.toLowerCase();
+
+		let num = abc.indexOf(v) + n;
+		if (num > 25) num %= abc.length;
+
+		let str = abc.substr(num, 1);
+
+		if (!str) str = 'a';
+		if (chk) str = str.toUpperCase();
+
+		obj.push(str);
+	});
+
+	return obj.join('');
+}
+
+function alphabet() {
+	let str = '';
+	for (let i = 97; i <= 122; i++) {
+		str += String.fromCharCode(i);
 	}
 
-	function alphabet() {
-		let str = '';
-		for (let i = 97; i <= 122; i++) {
-			str += String.fromCharCode(i);
-		}
-		return str;
-	}
+	return str;
+}
 
-	function isUpperCase(str) {
-		return str === str.toUpperCase();
-	}
+function isUpperCase(str) {
+	return str === str.toUpperCase();
 }
