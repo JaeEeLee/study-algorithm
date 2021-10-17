b5b961f3f02711eed17ab920d85a337ec3786cb2
LeeJaeEe <wodl0504@naver.com>
2021-10-17 20:31:05 +0900

    비밀지도

--- 202110/20211017_level1.js
+++ 202110/20211017_level1.js
@@ -1,18 +1,45 @@
-// #1
-// 나머지가 1인 수 찾기
-function solution(n) {
-    let i = 1
-    while(true){
-        if(n % i === 1) break
-        i++
-    }
-    return i
+//#1
+// [1차] 비밀지도
+function solution(n, arr1, arr2) {
+    let temp1 = toParseString(arr1, n)
+    let temp2 = toParseString(arr2, n)
+    let answer = []
+    temp1.map((v, i) => {
+        let str = '', j = 0
+        while (j < n) {
+            str += v[j] === '0' && v[j] === temp2[i][j] ? ' ' : '#'
+            j++
+        }
+        answer.push(str)
+    })
+    return answer
+
 }
 
+function toParseString(arr, n) {
+    let temp = []
+    arr.map((v) => {
+        let str = v.toString(2)
+        if (str.length < n) {
+            let i = '0', j = str.length
+            while (j < n - 1) {
+                i += '0'
+                j++
+            }
+            str = i + str
+        }
+        temp.push(str)
+    })
+    return temp
+}
+console.log(solution(5, [9, 20, 28, 18, 11], [30, 1, 21, 17, 28]))
 
-// #2
-// 나누어 떨어지는 수
-function solution(arr, divisor) {
-    let res = arr.filter((v) => v % divisor === 0)
-    return res.length ? res.sort((a, b) => a - b) : [-1]
+
+// 다른 사람 풀이 repeat, replace, 정규표현식 메모..
+function solutionOthers(n, arr1, arr2) {
+    return arr1.map((v, i) => addZero(n, (v | arr2[i]).toString(2)).replace(/1|0/g, a => +a ? '#' : ' '));
+}
+
+const addZero = (n, s) => {
+    return '0'.repeat(n - s.length) + s;
 }
